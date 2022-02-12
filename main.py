f0ed92fcbed8cdc66d00771a37254b2f61807aea
maksimum232@gmail.com <maksimum232@gmail.com>
2022-02-12 13:55:12 +0100

    Added login routing and register routing

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-import flask
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def root_route():
+    return "Hello World!"
+
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register_page():
+    return render_template("register.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
